6820d2194a58160ee194600663757b1eac714de6
nate.online <neutral gin>
2019-05-31 01:33:53 -0600

    2 player snake prototype working

---
+++ Assets/Input/PlayerInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public enum PlayerID { Player1 = 0, Player2 = 1 }
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerID playerID;
+    Player player;
+    PlayerController playerController;
+    Vector2Int moveDirection;
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer((int)playerID);
+        playerController = GetComponent<PlayerController>();
+        moveDirection = new Vector2Int(0,1);
+    }
+
+    void Update()
+    {
+        if (player.GetButtonDown("Move Up") && moveDirection.y != -1)
+        {
+            moveDirection.x = 0;
+            moveDirection.y = 1;
+        }
+
+        if (player.GetButtonDown("Move Right") && moveDirection.x != -1)
+        {
+            moveDirection.x = 1;
+            moveDirection.y = 0;
+        }
+
+        if (player.GetButtonDown("Move Down") && moveDirection.y != 1)
+        {
+            moveDirection.x = 0;
+            moveDirection.y = -1;
+        }
+
+        if (player.GetButtonDown("Move Left") && moveDirection.x != 1)
+        {
+            moveDirection.x = -1;
+            moveDirection.y = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        playerController.MovePlayer(moveDirection);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2Int playerLocation;
+    public GameObject bodyPart;
+
+    void Start()
+    {
+        playerLocation = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+    }
+
+    public void MovePlayer(Vector2Int moveDirection)
+    {
+        playerLocation += moveDirection;
+        GameObject.Instantiate(bodyPart, (Vector2)playerLocation, Quaternion.identity);
+    }
+}
